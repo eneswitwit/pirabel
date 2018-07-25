6af6b0f87fbde26e428b3cdfc0b8a89e97c6098a
Enes Witwit <witwit@stud.uni-heidelberg.de>
2018-07-25 22:33:50 +0200

    Added integration

--- pirabel.py
+++ pirabel.py
@@ -1,9 +1,23 @@
+import json
+from scipy.integrate import quad
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@app.route("/integrate/<fx>")
+def integrate(fx):
+    fx = convert_latex(fx);
+    return json.dumps(integration(fx));
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+def integration(fx):
+    area = quad(integrand(fx), 0, 1);
+    return area;
+
+def convert_latex(fx):
+    return fx.replace("^","**");
+
+def integrand(fx):
+    return eval("lambda x:" + fx);
