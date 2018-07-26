72d2b36af04361eda0c35ea8c6a12384306065e2
Enes Witwit <witwit@stud.uni-heidelberg.de>
2018-07-26 20:30:54 +0200

    Added indefinite integral and json serialization

--- pirabel.py
+++ pirabel.py
@@ -1,23 +1,54 @@
 import json
 from scipy.integrate import quad
+from sympy import integrate as indefinite
+from sympy import Symbol
 
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/integrate/<fx>")
-def integrate(fx):
+# routes
+@app.route("/integrate/<fx>/<down_boundary>/<upper_boundary>")
+def integrate_route(fx, down_boundary, upper_boundary):
     fx = convert_latex(fx);
-    return json.dumps(integration(fx)[0]);
+    indefinite_result = convert_python(str(indefinite(fx, Symbol('x'))));
+    integration_result = integration(fx, down_boundary, upper_boundary)[0];
+    json_result = [
+        ['indefinite', indefinite_result],
+        ['calculation', calculation(indefinite_result, down_boundary, upper_boundary)],
+        ['result', integration_result]
+    ];
+    return json.dumps(json_result);
+
+@app.route("/integrate/indefinite/<fx>")
+def indefinite_route(fx):
+    fx = convert_latex(fx);
+    indefinite_result = convert_python(str(indefinite(fx, Symbol('x'))));
+    json_result = [
+        ['indefinite', indefinite_result]
+    ];
+    return json.dumps(json_result);
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
-def integration(fx):
-    area = quad(integrand(fx), 0, 1);
+#integration
+def integration(fx, down_boundary, upper_boundary):
+    down_boundary = float(down_boundary);
+    upper_boundary = float(upper_boundary);
+    area = quad(integrand(fx), down_boundary, upper_boundary);
     return area;
-
-def convert_latex(fx):
-    return fx.replace("^","**");
 
 def integrand(fx):
     return eval("lambda x:" + fx);
+
+def calculation(fx, down_boundary, upper_boundary):
+    left = fx.replace("x", upper_boundary);
+    right = fx.replace("x", down_boundary);
+    return left + " - " + right;
+
+#helper functions 
+def convert_latex(fx):
+    return fx.replace("^","**");
+
+def convert_python(fx):
+    return fx.replace("**","^");    
