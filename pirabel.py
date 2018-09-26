bbe33144c8eec03999fc1852dc3e41e83436f5f2
Enes Witwit <witwit@stud.uni-heidelberg.de>
2018-09-26 23:05:42 +0100

    Refactored

--- pirabel.py
+++ pirabel.py
@@ -7,65 +7,75 @@ from sympy import Symbol
 
 # flask
 from flask import Flask
+
 app = Flask(__name__)
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
+
 # routes
 @app.route("/")
 def home():
-    return json.dumps("Pirabel API 1.0");
+    return json.dumps("Pirabel API 1.0")
+
 
 # integration routes
 @app.route("/integrate/<fx>/<down_boundary>/<upper_boundary>")
 def integrate_route(fx, down_boundary, upper_boundary):
     fx = convert_latex(fx);
-    indefinite_result = convert_python(str(indefinite(fx, Symbol('x'))));
-    integration_result = integration(fx, down_boundary, upper_boundary)[0];
+    indefinite_result = convert_python(str(indefinite(fx, Symbol('x'))))
+    integration_result = integration(fx, down_boundary, upper_boundary)[0]
     json_result = [
         {'indefinite': indefinite_result},
         {'calculation': calculation(indefinite_result, down_boundary, upper_boundary)},
         {'result': integration_result}
     ];
-    return json.dumps(json_result);
+    return json.dumps(json_result)
+
 
 @app.route("/integrate/indefinite/<fx>")
 def indefinite_route(fx):
-    fx = convert_latex(fx);
-    indefinite_result = convert_python(str(indefinite(fx, Symbol('x'))));
+    fx = convert_latex(fx)
+    indefinite_result = convert_python(str(indefinite(fx, Symbol('x'))))
     json_result = [
         {'indefinite': indefinite_result}
     ];
-    return json.dumps(json_result);
+    return json.dumps(json_result)
+
 
 # differentation routes
 @app.route("/differentiate/<fx>")
 def differentiate_route(fx):
-    fx = convert_latex(fx);
+    fx = convert_latex(fx)
     differentation_result = convert_python(str(diff(fx, Symbol('x'))));
     json_result = [
-        {'derivative' :differentation_result}
-    ];
-    return json.dumps(json_result);
+        {'derivative': differentation_result}
+    ]
+    return json.dumps(json_result)
 
-#integration
+
+# integration
 def integration(fx, down_boundary, upper_boundary):
-    down_boundary = float(down_boundary);
-    upper_boundary = float(upper_boundary);
-    area = quad(integrand(fx), down_boundary, upper_boundary);
+    down_boundary = float(down_boundary)
+    upper_boundary = float(upper_boundary)
+    area = quad(integrand(fx), down_boundary, upper_boundary)
     return area;
 
+
 def integrand(fx):
-    return eval("lambda x:" + fx);
+    return eval("lambda x:" + fx)
+
 
 def calculation(fx, down_boundary, upper_boundary):
-    left = fx.replace("x", upper_boundary);
-    right = fx.replace("x", down_boundary);
-    return left + " - " + right;
+    left = fx.replace("x", upper_boundary)
+    right = fx.replace("x", down_boundary)
+    return left + " - " + right
 
-#helper functions 
+
+# helper functions
 def convert_latex(fx):
-    return fx.replace("^","**");
+    return fx.replace("^", "**")
+
 
 def convert_python(fx):
-    return fx.replace("**","^");    
+    return fx.replace("**", "^")
