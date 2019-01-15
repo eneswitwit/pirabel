508cd0e2e7d53cd3078023e4c93933911313bd25
Enes Witwit <witwit@stud.uni-heidelberg.de>
2019-01-15 12:43:59 +0000

    Implemented Roman

--- pirabel.py
+++ pirabel.py
@@ -214,6 +214,32 @@ def fraction(firstFactor, firstCounter, firstDenominator, operator, secondFactor
 
     return json.dumps(json_result)
 
+# convert int to roman numbers
+@app.route("/roman/<num>")
+def convertToRomam(num):
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    roman_num = ''
+    i = 0
+    while  num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syb[i]
+            num -= val[i]
+            i += 1
+    return roman_num
+
+
+
 # helper functions
 def convert_latex(fx):
     fx = fx.replace("{", "(")
